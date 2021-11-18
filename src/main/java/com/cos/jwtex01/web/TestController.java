521b3f44f70f5d43ace2253df3ddab33ec2ed213
rlagudals95 <76252074+rlagudals95@users.noreply.github.com>
2021-11-18 20:23:38 +0900

    searchResponse

--- src/main/java/com/cos/jwtex01/web/TestController.java
+++ src/main/java/com/cos/jwtex01/web/TestController.java
@@ -2,6 +2,7 @@ package com.cos.jwtex01.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.jwtex01.config.auth.LoginUser;
@@ -14,11 +15,11 @@ public class TestController {
 	private NaverService naverService;
 	
 	// 유저 혹은 어드민이 접근 가능
-	@GetMapping("/search/{url}")
+	@PostMapping("/search/{url}")
 	public String user(@PathVariable("url") String url) {
 		//System.out.println("principal : "+principal.getUsername());
-		NaverService.main(url);	
-		return "연결완료";
+			
+		return NaverService.main(url);
 	}	
 }
 
